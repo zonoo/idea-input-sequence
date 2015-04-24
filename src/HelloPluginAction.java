5d6b73be1b10cf661977748cae2fe5e2e681be4e
ishizono <t.ishizono@futurek.info>
2015-04-24 15:31:29 +0900

    Initial commit.

---
+++ src/HelloPluginAction.java
@@ -0,0 +1,100 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.command.CommandProcessor;
+import com.intellij.openapi.editor.Caret;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by ishizono on 15/04/24.
+ */
+public class HelloPluginAction extends AnAction {
+    public void actionPerformed(AnActionEvent e) {
+        final Project project = e.getProject();
+        if (project == null) {
+            return;
+        }
+        Editor editor = FileEditorManager.getInstance(project).getSelectedTextEditor();
+        if (editor == null) {
+            return;
+        }
+
+        // カレントのキャレットから連番にする
+        // カレントキャレットの位置を取得する
+        List<Caret> allCarets = editor.getCaretModel().getAllCarets();
+        HashMap map = new HashMap();
+        for(Caret caret : allCarets){
+            int currentCaret = caret.getOffset();
+//            System.out.println(currentCaret);
+//            Notifications.Bus.notify(
+//                    new Notification("sample", "Hello Plugin!", "Hello! This is Sample Plugin." + Integer.toString(currentCaret), NotificationType.INFORMATION)
+//            );
+            map.put(Integer.toString(currentCaret), currentCaret);
+        }
+        final Document document = editor.getDocument();
+        VirtualFile virtualFile = FileDocumentManager.getInstance().getFile(document);
+        if (virtualFile == null) {
+            return;
+        }
+        final String contents;
+        // ドキュメントを読み込み、文字列を StringBuffer で組み立てる
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(virtualFile.getPath()));
+            String currentLine;
+            StringBuilder sb = new StringBuilder();
+            int counter = 0;
+            int editNum = 0;
+            while ((currentLine = br.readLine()) != null) {
+                char[] chars = currentLine.toCharArray();
+                // 文字列を検索し、挿入位置を特定しその前に番号をいれていく
+                for(char character : chars){
+                    if(map.containsKey(Integer.toString(counter))){
+                        sb.append(editNum);
+                        sb.append(character);
+                        editNum++;
+                    } else {
+                        sb.append(character);
+                    }
+                    counter++;
+                }
+                if(map.containsKey(Integer.toString(counter))){
+                    sb.append(editNum);
+                    editNum++;
+                }
+                sb.append("\n");
+                counter++;
+            }
+            contents = sb.toString();
+        } catch (IOException e1) {
+            return;
+        }
+        final Runnable readRunner = new Runnable() {
+            @Override
+            public void run() {
+                document.setText(contents);
+            }
+        };
+        ApplicationManager.getApplication().invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                CommandProcessor.getInstance().executeCommand(project, new Runnable() {
+                    @Override
+                    public void run() {
+                        ApplicationManager.getApplication().runWriteAction(readRunner);
+                    }
+                }, "DiskRead", null);
+            }
+        });
+    }
+}
